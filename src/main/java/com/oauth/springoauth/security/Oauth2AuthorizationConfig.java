0947c713327c8f6eb563a667e2d901bb9a4ba509
freeefly <freefly3557@gmail.com>
2020-04-27 14:57:25 +0900

    feat : rsa 방식의 secret key 교환방식으로 변경

--- src/main/java/com/oauth/springoauth/security/Oauth2AuthorizationConfig.java
+++ src/main/java/com/oauth/springoauth/security/Oauth2AuthorizationConfig.java
@@ -1,19 +1,21 @@
 package com.oauth.springoauth.security;
 
 import com.oauth.springoauth.user.CustomUserDetailsService;
+import javassist.ClassPath;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import javax.sql.DataSource;
 
@@ -26,8 +28,8 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
     private final PasswordEncoder passwordEncoder;
     private final CustomUserDetailsService userDetailsService;
 
-    @Value("${security.oauth2.jwt.signkey}")
-    private String signKey;
+//    @Value("${security.oauth2.jwt.signkey}")
+//    private String signKey;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -49,15 +51,16 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
         ;
     }
 
-        @Override
+    @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         super.configure(endpoints);
         endpoints.accessTokenConverter(jwtAccessTokenConverter()).userDetailsService(userDetailsService);
     }
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("oauth2jwt.jks"), "oauth2jwtpass".toCharArray());
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey(signKey);
+        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("oauth2jwt"));
         return converter;
     }
 }
